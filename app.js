3a3610a6d20b7eac18d13e21759f8c5038ea0e8c
dejotb <dejotb@gmail.comclear>
2021-11-18 22:07:48 +0100

    first commit

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const nav = document.querySelector('.nav')
 
 
 const toggleHam = function (e) {
-
-    if (e.target.hasAttribute('aria-expanded') || e.target.classList.contains('nav__link')) {
+    console.log(e.target);
+console.log(e.target.parentElement.hasAttribute('aria-expanded'));
+    if (e.target.hasAttribute('aria-expanded') || e.target.parentElement.hasAttribute('aria-expanded') || e.target.classList.contains('nav__link')) {
         const eState = e.target.getAttribute('aria-expanded')
 
         if (eState == 'true' || e.target.classList.contains('nav__link')) {

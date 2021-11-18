eff6772bad2943606ce602edfb610f8dba24b050
dejotb <dejotb@gmail.comclear>
2021-11-18 22:01:05 +0100

    first commit

--- app.js
+++ app.js
@@ -18,38 +18,41 @@ const nav = document.querySelector('.nav')
 
 
 const toggleHam = function (e) {
-    // console.log(e.currentTarget.children.attribute.contains('aria-expanded'))
-    const eState = e.target.getAttribute('aria-expanded')
+    console.log(e.target.hasAttribute('aria-expanded'));
+    if (e.target.hasAttribute('aria-expanded') || e.target.classList.contains('nav__link')) {
+        const eState = e.target.getAttribute('aria-expanded')
 
-    if (eState == 'true' || e.target.classList.contains('nav__link')) {
-        burger.classList.remove('toggleHam')
-        navListWrapper.style.transform = 'translateX(-250%)'
-        navList.style.transform = 'translateX(-100%)'
-        burgerText.classList.add('hamburger__active')
-        burgerText.classList.remove('hamburger__inactive')
-        setTimeout( () => burgerText.textContent = 'MENU', 400)
+        if (eState == 'true' || e.target.classList.contains('nav__link')) {
+            burger.classList.remove('toggleHam')
+            navListWrapper.style.transform = 'translateX(-250%)'
+            navList.style.transform = 'translateX(-100%)'
+            burgerText.classList.add('hamburger__active')
+            burgerText.classList.remove('hamburger__inactive')
+            setTimeout( () => burgerText.textContent = 'MENU', 400)
 
 
 
-        nav.querySelectorAll('[aria-expanded]').forEach(element => {
-            element.setAttribute('aria-expanded', false);
-        });
+            nav.querySelectorAll('[aria-expanded]').forEach(element => {
+                element.setAttribute('aria-expanded', false);
+            });
 
-    } else {
-        burger.classList.add('toggleHam')
-        navListWrapper.style.transform = 'translateX(0)'
-        navList.style.transform = 'translateX(0)'
-        burgerText.classList.remove('hamburger__active')
-        burgerText.classList.add('hamburger__inactive')
-        setTimeout( () => burgerText.textContent = 'ZAMKNIJ', 400)
+        } else {
+            burger.classList.add('toggleHam')
+            navListWrapper.style.transform = 'translateX(0)'
+            navList.style.transform = 'translateX(0)'
+            burgerText.classList.remove('hamburger__active')
+            burgerText.classList.add('hamburger__inactive')
+            setTimeout( () => burgerText.textContent = 'ZAMKNIJ', 400)
 
-        nav.querySelectorAll('[aria-expanded]').forEach(element => {
-            element.setAttribute('aria-expanded', true);
-        });
+            nav.querySelectorAll('[aria-expanded]').forEach(element => {
+                element.setAttribute('aria-expanded', true);
+            });
 
+        }
     }
+
 }
 
 nav.addEventListener('click', toggleHam)
-// navListWrapper.addEventListener('click', toggleHam)
+// burger.addEventListener('click', toggleHam)
 

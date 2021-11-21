3c9cb608839c7fcf406eded776ed5d7215429d32
dejotb <dejotb@gmail.comclear>
2021-11-21 22:19:30 +0100

    navigation clearance

--- app.js
+++ app.js
@@ -1,59 +1,53 @@
-const burger = document.querySelector('.hamburger')
-const burgerLine = document.querySelector('.hamburger__line')
-const burgerText = document.querySelector('.hamburger__text')
-const navListWrapper = document.querySelector('.nav__list--wrapper')
-const navList = document.querySelector('.nav__list')
-const nav = document.querySelector('.nav')
+"use strict";
 
+// Navigation
 
+const navigation = function () {
+    const burger = document.querySelector('.hamburger')
+    const burgerText = document.querySelector('.hamburger__text')
+    const nav = document.querySelector('.nav')
+    const navListOverlay = document.querySelector('.nav__list--overlay')
+    const navList = document.querySelector('.nav__list')
 
+    const navAriaExpandedState = function (state) {
+        nav.querySelectorAll('[aria-expanded]').forEach(element => {
+            element.setAttribute('aria-expanded', state);
+        })
+    }
 
+    const navTranslate = function (navOverlayPosition, navListPosition) {
+        navListOverlay.style.transform = 'translateX(-150%)'
+        navList.style.transform = 'translateX(-100%)'
+        navListOverlay.style.transform = `translateX(${navOverlayPosition}%)`
+        navList.style.transform = `translateX(${navListPosition}%)`
+    }
 
-// toggle hamburger
+    navTranslate()
 
+    const toggleHam = function (e) {
+        if (e.target.hasAttribute('aria-expanded') || e.target.classList.contains('hamburger__line') || e.target.classList.contains('nav__link')) {
+            const eState = burger.getAttribute('aria-expanded')
 
-// console.log([...e.currentTarget.children].some(item => item.hasAttribute('aria-expanded')));
-    // console.log(e.target.classList.contains('nav__link'));
+            if (eState === 'true' || e.target.classList.contains('nav__link')) {
+                burger.classList.remove('toggleHam')
+                burgerText.classList.add('hamburger__active')
+                burgerText.classList.remove('hamburger__inactive')
+                setTimeout( () => burgerText.textContent = 'MENU', 400)
+                navTranslate(-150, -100)
+                navAriaExpandedState(false)
 
-
-
-const toggleHam = function (e) {
-    console.log(e.target);
-console.log(e.target.classList.contains('hamburger__line'));
-    if (e.target.hasAttribute('aria-expanded') || e.target.classList.contains('hamburger__line') || e.target.classList.contains('nav__link')) {
-        const eState = burger.getAttribute('aria-expanded')
-
-        if (eState == 'true' || e.target.classList.contains('nav__link')) {
-            burger.classList.remove('toggleHam')
-            navListWrapper.style.transform = 'translateX(-250%)'
-            navList.style.transform = 'translateX(-100%)'
-            burgerText.classList.add('hamburger__active')
-            burgerText.classList.remove('hamburger__inactive')
-            setTimeout( () => burgerText.textContent = 'MENU', 400)
-
-
-
-            nav.querySelectorAll('[aria-expanded]').forEach(element => {
-                element.setAttribute('aria-expanded', false);
-            });
-
-        } else {
-            burger.classList.add('toggleHam')
-            navListWrapper.style.transform = 'translateX(0)'
-            navList.style.transform = 'translateX(0)'
-            burgerText.classList.remove('hamburger__active')
-            burgerText.classList.add('hamburger__inactive')
-            setTimeout( () => burgerText.textContent = 'ZAMKNIJ', 400)
-
-            nav.querySelectorAll('[aria-expanded]').forEach(element => {
-                element.setAttribute('aria-expanded', true);
-            });
-
+            } else {
+                burger.classList.add('toggleHam')
+                burgerText.classList.remove('hamburger__active')
+                burgerText.classList.add('hamburger__inactive')
+                setTimeout( () => burgerText.textContent = 'ZAMKNIJ', 400)
+                navTranslate(0, 0)
+                navAriaExpandedState(true)
+            }
         }
     }
 
+    nav.addEventListener('click', toggleHam)
 }
 
-nav.addEventListener('click', toggleHam)
-// burger.addEventListener('click', toggleHam)
-
+navigation()

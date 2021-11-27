13f657da74cc1c4a4d09ead39f15f73e5dc64123
dejotb <dejotb@gmail.comclear>
2021-11-27 12:12:03 +0100

    changed header

--- app.js
+++ app.js
@@ -49,3 +49,57 @@ const navigation = function () {
 }
 
 navigation()
+
+
+
+// Image docking
+
+
+const products = document.querySelector('.products')
+const image = document.querySelector('.img__scrolled');
+// console.log(image);
+
+
+const movingImage = function (entries) {
+    let entry = entries[0];
+    entries.forEach(entry => console.log(entry))
+    if (!entry.isIntersecting) {
+        // image.classList.add('header__img')
+        image.classList.remove('img')
+        image.classList.add('img__scrolled')
+    } else {
+        // image.classList.remove('header__img')
+        image.classList.add('img')
+        image.classList.remove('img__scrolled')
+    }
+}
+
+
+const productsOptions = {
+    root: null,
+    threshold: [0.18],
+    rootMargin: '0px',
+}
+
+const productsObserver = new IntersectionObserver(movingImage, productsOptions)
+
+productsObserver.observe(products)
+
+
+let lastKnownScrollPosition = 0
+
+const productScroll = function() {
+    window.addEventListener('scroll', function (e) {
+        lastKnownScrollPosition = e.currentTarget.pageYOffset
+        // console.log(lastKnownScrollPosition);
+        // image.style.transform = `translateY('${lastKnownScrollPosition}px')`
+        // image.style.transform = `translate3d(0, ${lastKnownScrollPosition/3 -400}px ,0) `
+        image.style.transform = `scale(${lastKnownScrollPosition/500})`
+        console.log(`-${1 + lastKnownScrollPosition/150}`);
+        // image.style.transform = `martix(${lastKnownScrollPosition} -200px, 0, 0, ${lastKnownScrollPosition } -200px, 0,1 })`
+
+    })
+
+}
+
+// productScroll()

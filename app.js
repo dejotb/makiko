56caf4c9dbc39580bfb5bc545d1d323b7c384c9d
dejotb <dejotb@gmail.comclear>
2021-11-28 17:03:31 +0100

    added composition options

--- app.js
+++ app.js
@@ -62,14 +62,14 @@ const image = document.querySelector('.img__scrolled');
 
 const movingImage = function (entries) {
     let entry = entries[0];
-    entries.forEach(entry => console.log(entry))
+    // entries.forEach(entry => console.log(entry))
     if (!entry.isIntersecting) {
         // image.classList.add('header__img')
-        image.classList.remove('img')
+        image.classList.remove('img-1')
         image.classList.add('img__scrolled')
     } else {
         // image.classList.remove('header__img')
-        image.classList.add('img')
+        image.classList.add('img-1')
         image.classList.remove('img__scrolled')
     }
 }
@@ -103,3 +103,105 @@ const productScroll = function() {
 }
 
 // productScroll()
+
+
+
+// composition switch
+
+const box = document.querySelector('.box')
+
+const infoType = document.querySelectorAll('.box__card--composition')
+console.log(infoType);
+
+const button = document.querySelectorAll('.btn')
+
+console.log(button);
+
+const cardIngredients = document.querySelectorAll('.box__card--composition-ingredients');
+
+const cardNutricients = document.querySelectorAll('.box__card--composition-nutrition');
+
+
+
+    box.addEventListener('click', function(e) {
+
+        if ([...e.target.parentElement.children].find(el => el.classList.contains('box__card--composition-nutrition'))) {
+            return
+        } else  {
+            if (e.target.classList.contains('products__btn--nutritients')          ) {
+                const cardNumber = e.target.parentElement.dataset.boxcomposition;
+                const NutritionTable = `
+                <div
+                class="box__card--composition-nutrition"
+                data-nutrition="1"
+              >
+                <table>
+                <thead>
+                  <th scope="col">Tabela</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      Wartość odżywcza w /
+                      <br />
+                      Nutritional value per
+                    </th>
+                    <td>100g</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Wartość energetyczna / Energy</th>
+                    <td>
+                      1811kJ
+                      <br />
+                      433kcal
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      Tłuszcz / Fat
+                      <br />
+                      w tym kwasy tłuszczowe nasycone / including saturated
+                      fatty acids
+                    </th>
+                    <td>
+                      17.8g
+                      <br />
+                      3.2g
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      Węglowodany / Carbohydrates
+                      <br />
+                      w tym cukry / including sugars
+                    </th>
+                    <td>
+                      59.3g
+                      <br />
+                      6.9g
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Błonnik / Fibre</th>
+                    <td>3.2g</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Białko / Protein</th>
+                    <td>11.7g</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Sól / Salt</th>
+                    <td>0.04g</td>
+                  </tr>
+                </tbody>
+              </table>
+              </div>
+                `
+                e.target.parentElement.insertAdjacentHTML('beforeend', NutritionTable);
+
+
+            }
+        }
+
+
+    })

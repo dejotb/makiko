5bbbd3dff0b506582de969071ca7bcc64ccd4fea
dejotb <dejotb@gmail.comclear>
2021-11-29 19:34:39 +0100

    products section composition functionality added

--- app.js
+++ app.js
@@ -1,227 +1,275 @@
-"use strict";
-
 // Navigation
 
-const navigation = function () {
-    const burger = document.querySelector('.hamburger')
-    const burgerText = document.querySelector('.hamburger__text')
-    const nav = document.querySelector('.nav')
-    const navListOverlay = document.querySelector('.nav__list--overlay')
-    const navListWrapper = document.querySelector('.nav__list--wrapper')
+const navigationSwitch = function () {
+  const burger = document.querySelector('.hamburger');
+  const burgerText = document.querySelector('.hamburger__text');
+  const nav = document.querySelector('.nav');
+  const navListOverlay = document.querySelector('.nav__list--overlay');
+  const navListWrapper = document.querySelector('.nav__list--wrapper');
 
-    const navAriaExpandedState = function (state) {
-        nav.querySelectorAll('[aria-expanded]').forEach(element => {
-            element.setAttribute('aria-expanded', state);
-        })
+  const navAriaExpandedState = function (state) {
+    nav.querySelectorAll('[aria-expanded]').forEach((element) => {
+      element.setAttribute('aria-expanded', state);
+    });
+  };
+
+  const navTranslate = function (navOverlayPosition, navListPosition) {
+    navListOverlay.style.transform = `translateX(${navOverlayPosition}%)`;
+    navListWrapper.style.transform = `translateX(${navListPosition}%)`;
+  };
+
+  navTranslate();
+
+  const toggleHam = function (e) {
+    if (
+      e.target.hasAttribute('aria-expanded') ||
+      e.target.classList.contains('hamburger__line') ||
+      e.target.classList.contains('nav__link')
+    ) {
+      const eState = burger.getAttribute('aria-expanded');
+
+      if (eState === 'true' || e.target.classList.contains('nav__link')) {
+        burger.classList.remove('toggleHam');
+        burgerText.classList.add('hamburger__active');
+        burgerText.classList.remove('hamburger__inactive');
+        setTimeout(() => (burgerText.textContent = 'MENU'), 400);
+        navTranslate(-150, -100);
+        navAriaExpandedState(false);
+      } else {
+        burger.classList.add('toggleHam');
+        burgerText.classList.remove('hamburger__active');
+        burgerText.classList.add('hamburger__inactive');
+        setTimeout(() => (burgerText.textContent = 'ZAMKNIJ'), 400);
+        navTranslate(0, 0);
+        navAriaExpandedState(true);
+      }
     }
+  };
 
-    const navTranslate = function (navOverlayPosition, navListPosition) {
-        navListOverlay.style.transform = `translateX(${navOverlayPosition}%)`
-        navListWrapper.style.transform = `translateX(${navListPosition}%)`
-    }
+  nav.addEventListener('click', toggleHam);
+};
 
-    navTranslate()
-
-    const toggleHam = function (e) {
-        if (e.target.hasAttribute('aria-expanded') || e.target.classList.contains('hamburger__line') || e.target.classList.contains('nav__link')) {
-            const eState = burger.getAttribute('aria-expanded')
-
-            if (eState === 'true' || e.target.classList.contains('nav__link')) {
-                burger.classList.remove('toggleHam')
-                burgerText.classList.add('hamburger__active')
-                burgerText.classList.remove('hamburger__inactive')
-                setTimeout( () => burgerText.textContent = 'MENU', 400)
-                navTranslate(-150, -100)
-                navAriaExpandedState(false)
-
-            } else {
-                burger.classList.add('toggleHam')
-                burgerText.classList.remove('hamburger__active')
-                burgerText.classList.add('hamburger__inactive')
-                setTimeout( () => burgerText.textContent = 'ZAMKNIJ', 400)
-                navTranslate(0, 0)
-                navAriaExpandedState(true)
-            }
-        }
-    }
-
-    nav.addEventListener('click', toggleHam)
-}
-
-navigation()
-
-
+navigationSwitch();
 
 // Image docking
 
-
-const products = document.querySelector('.products')
+const products = document.querySelector('.products');
 const image = document.querySelector('.img__scrolled');
 // console.log(image);
 
-
 const movingImage = function (entries) {
-    let entry = entries[0];
-    // entries.forEach(entry => console.log(entry))
-    if (!entry.isIntersecting) {
-        // image.classList.add('header__img')
-        image.classList.remove('img-1')
-        image.classList.add('img__scrolled')
-    } else {
-        // image.classList.remove('header__img')
-        image.classList.add('img-1')
-        image.classList.remove('img__scrolled')
-    }
-}
-
+  const entry = entries[0];
+  // entries.forEach(entry => console.log(entry))
+  if (!entry.isIntersecting) {
+    // image.classList.add('header__img')
+    image.classList.remove('img-1');
+    image.classList.add('img__scrolled');
+  } else {
+    // image.classList.remove('header__img')
+    image.classList.add('img-1');
+    image.classList.remove('img__scrolled');
+  }
+};
 
 const productsOptions = {
-    root: null,
-    threshold: [0.195],
-    rootMargin: '0px',
-}
+  root: null,
+  threshold: [0.195],
+  rootMargin: '0px',
+};
 
-const productsObserver = new IntersectionObserver(movingImage, productsOptions)
+const productsObserver = new IntersectionObserver(movingImage, productsOptions);
 
-productsObserver.observe(products)
+// productsObserver.observe(products);
 
+let lastKnownScrollPosition = 0;
 
-let lastKnownScrollPosition = 0
-
-const productScroll = function() {
-    window.addEventListener('scroll', function (e) {
-        lastKnownScrollPosition = e.currentTarget.pageYOffset
-        // console.log(lastKnownScrollPosition);
-        // image.style.transform = `translateY('${lastKnownScrollPosition}px')`
-        // image.style.transform = `translate3d(0, ${lastKnownScrollPosition/3 -400}px ,0) `
-        image.style.transform = `scale(${lastKnownScrollPosition/500})`
-        console.log(`-${1 + lastKnownScrollPosition/150}`);
-        // image.style.transform = `martix(${lastKnownScrollPosition} -200px, 0, 0, ${lastKnownScrollPosition } -200px, 0,1 })`
-
-    })
-
-}
+const productScroll = function () {
+  window.addEventListener('scroll', (e) => {
+    lastKnownScrollPosition = e.currentTarget.pageYOffset;
+    // console.log(lastKnownScrollPosition);
+    // image.style.transform = `translateY('${lastKnownScrollPosition}px')`
+    // image.style.transform = `translate3d(0, ${lastKnownScrollPosition/3 -400}px ,0) `
+    image.style.transform = `scale(${lastKnownScrollPosition / 500})`;
+    console.log(`-${1 + lastKnownScrollPosition / 150}`);
+    // image.style.transform = `martix(${lastKnownScrollPosition} -200px, 0, 0, ${lastKnownScrollPosition } -200px, 0,1 })`
+  });
+};
 
 // productScroll()
 
+// Product section composition switch
 
+const compositionSwitch = function () {
+  const box = document.querySelector('.products__box');
+  const productDetails = document.querySelectorAll('.products__composition');
 
-// composition switch
+  const nutritionsOptions = {
+    1: [
+      '1811kJ',
+      '433kcal',
+      '17.8g',
+      '3.2g',
+      '59.3g',
+      '6.9g',
+      '3.2g',
+      '11.7g',
+      '0.04g',
+    ],
+    2: [
+      '1673kJ',
+      '400kcal',
+      '11.3g',
+      '2.7g',
+      '67.9g',
+      '8g',
+      '4.2g',
+      '10.2g',
+      '0.04g',
+    ],
+    3: [
+      '1524kJ',
+      '365kcal',
+      '6.85g',
+      '2.2g',
+      '68.1g',
+      '6.4g',
+      '2g',
+      '8.7g',
+      '6.63g',
+    ],
+  };
 
-const box = document.querySelector('.box')
+  const nutritientsGenerator = function (nutritionData) {
+    return `
+    <div
+    class="products__composition--nutrition"
+    data-nutrition="1"
+    >
+    <table>
 
-const productDetails = document.querySelectorAll('.box__card--composition')
+    <tbody>
+      <tr>
+        <th scope="row">
+          Wartość odżywcza w
+        </th>
+        <td>100g</td>
+      </tr>
+      <tr>
+        <th scope="row">Wartość energetyczna</th>
+        <td>
+          ${nutritionData[0]}
+          <br />
+          ${nutritionData[1]}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          Tłuszcz
+          <br />
+          w tym kwasy tłuszczowe nasycone
+        </th>
+        <td>
+        ${nutritionData[2]}
+          <br />
+          ${nutritionData[3]}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          Węglowodany
+          <br />
+          w tym cukry
+        </th>
+        <td>
+        ${nutritionData[4]}
+          <br />
+          ${nutritionData[5]}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Błonnik</th>
+        <td>${nutritionData[6]}</td>
+      </tr>
+      <tr>
+        <th scope="row">Białko</th>
+        <td>${nutritionData[7]}</td>
+      </tr>
+      <tr>
+        <th scope="row">Sól</th>
+        <td>${nutritionData[8]}</td>
+      </tr>
+    </tbody>
+    </table>
+    </div>
+    `;
+  };
 
-const NutritionTable = `
-<div
-class="box__card--composition-nutrition"
-data-nutrition="1"
->
-<table>
-<thead>
-  <th scope="col">Tabela</th>
-</thead>
-<tbody>
-  <tr>
-    <th scope="row">
-      Wartość odżywcza w /
-      <br />
-      Nutritional value per
-    </th>
-    <td>100g</td>
-  </tr>
-  <tr>
-    <th scope="row">Wartość energetyczna / Energy</th>
-    <td>
-      1811kJ
-      <br />
-      433kcal
-    </td>
-  </tr>
-  <tr>
-    <th scope="row">
-      Tłuszcz / Fat
-      <br />
-      w tym kwasy tłuszczowe nasycone / including saturated
-      fatty acids
-    </th>
-    <td>
-      17.8g
-      <br />
-      3.2g
-    </td>
-  </tr>
-  <tr>
-    <th scope="row">
-      Węglowodany / Carbohydrates
-      <br />
-      w tym cukry / including sugars
-    </th>
-    <td>
-      59.3g
-      <br />
-      6.9g
-    </td>
-  </tr>
-  <tr>
-    <th scope="row">Błonnik / Fibre</th>
-    <td>3.2g</td>
-  </tr>
-  <tr>
-    <th scope="row">Białko / Protein</th>
-    <td>11.7g</td>
-  </tr>
-  <tr>
-    <th scope="row">Sól / Salt</th>
-    <td>0.04g</td>
-  </tr>
-</tbody>
-</table>
-</div>
-`
-const ingredients = `
-            <div
-                class="box__card--composition-ingredients"
-                data-ingredients="1"
-              >
-                <p>Składniki</p>
-                <ul>
-                  <li>mąka pszenna</li>
-                  <li>ziarno sezamowe (20%)</li>
-                  <li>tłuszcz roślinny</li>
-                  <li>cukier</li>
-                  <li>drożdże</li>
-                  <li>wodorotlenek sodu (regulator kwasowości)</li>
-                </ul>
+  const ingredientsOptions = {
+    1: 'ziarno sezamowe (20%)',
+    2: 'mak (3%)',
+    3: 'sól (2%)',
+  };
 
-                <!-- <p>Ingredients</p>
-                <ul>
-                  <li>wheat flour</li>
-                  <li>sesame seeds (20%)</li>
-                  <li>vegetable fat</li>
-                  <li>sugar</li>
-                  <li>yeast</li>
-                  <li>sodium hydroxide</li>
-                </ul> -->
-              </div>
-            </div>`
+  const ingredientsGenerator = function (ingredient) {
+    return `
+    <div
+        class="products__composition--ingredients"
+        data-ingredients="1"
+      >
+        <p>Składniki:</p>
+        <ul>
+          <li>mąka pszenna,</li>
+          <li>${ingredient},</li>
+          <li>tłuszcz roślinny,</li>
+          <li>cukier,</li>
+          <li>drożdże,</li>
+          <li>wodorotlenek sodu (regulator kwasowości).</li>
+        </ul>
+      </div>
+    </div>`;
+  };
 
+  productDetails.forEach((product) => {
+    product.insertAdjacentHTML(
+      'beforeend',
+      ingredientsGenerator(
+        ingredientsOptions[`${product.dataset.boxcomposition}`]
+      )
+    );
+  });
 
-productDetails.forEach(product => product.insertAdjacentHTML('beforeend', ingredients))
-
-
-const switchData = function (e, HTMLOption, cardType) {
+  const switchData = function (e, HTMLOption, cardType) {
     e.target.parentElement.insertAdjacentHTML('beforeend', HTMLOption);
-    e.target.parentElement.querySelector(cardType).remove()
-}
+    e.target.parentElement.querySelector(cardType).remove();
+  };
 
-box.addEventListener('click', function(e) {
-        if (e.target.classList.contains('products__btn--nutritients') && !e.target.parentElement.querySelector('.box__card--composition-nutrition')) {
-            // const cardNumber = e.target.parentElement.dataset.boxcomposition;
-            switchData(e, NutritionTable, '.box__card--composition-ingredients')
-
-        } else if (e.target.classList.contains('products__btn--ingredients') && !e.target.parentElement.querySelector('.box__card--composition-ingredients')){
-            switchData(e, ingredients, '.box__card--composition-nutrition')
-        }
+  box.addEventListener('click', (e) => {
+    if (
+      e.target.classList.contains('products__btn--nutritients') &&
+      !e.target.parentElement.querySelector('.products__composition--nutrition')
+    ) {
+      switchData(
+        e,
+        nutritientsGenerator(
+          nutritionsOptions[`${e.target.parentElement.dataset.boxcomposition}`]
+        ),
+        '.products__composition--ingredients'
+      );
+    } else if (
+      e.target.classList.contains('products__btn--ingredients') &&
+      !e.target.parentElement.querySelector(
+        '.products__composition--ingredients'
+      )
+    ) {
+      switchData(
+        e,
+        ingredientsGenerator(
+          ingredientsOptions[`${e.target.parentElement.dataset.boxcomposition}`]
+        ),
+        '.products__composition--nutrition'
+      );
     }
-)
+  });
+};
+
+compositionSwitch();
